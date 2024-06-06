5905ed3f42a8c9c0e453740c4ecd1059c7f436f8
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-06 20:46:53 +0530

    add auth app url

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/", include("auth.urls")),
+    path("api/v1/auth/", include("authentication.urls")),
 ]
