cbd262fe13adada1051979ff8b9757394f57b27f
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-06 20:48:42 +0530

    add custom user model

---
+++ server/authentication/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from datetime import timedelta
+import os
+from django.utils import timezone
+import jwt
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    MY_CHOICES = [
+        ("ADMIN", "Admin"),
+        ("GUEST", "Guest"),
+    ]
+
+    email = models.EmailField(
+        verbose_name="Email",
+        max_length=100,
+        unique=True,
+    )
+    username = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    role = models.CharField(max_length=10, choices=MY_CHOICES, default="GUEST")
+    refresh_token = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    # save username into DB while registering (slicing email)
+    def save(self, *args, **kargs):
+        pass
+
+
+class VerificationLink(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    verification_token = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def generate_token(cls, user_id):
+        if not user_id:
+            return "User id not found"
+
+        user = User.objects.get(id=user_id)
+        if not user:
+            return "User does not exist"
+
+        encoded_token = jwt.encode(
+            {"id": user_id}, os.environ.get("VERIFICATION_SECRET"), algorithm="HS256"
+        )
+        token = cls.objects.create(token=encoded_token, user=user_id)
+        if token:
+            return token
+        else:
+            return "Error occurred while creating token"
