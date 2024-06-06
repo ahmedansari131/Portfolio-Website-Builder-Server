15461deafb7df074f287a9cba5d10de0f912b89a
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-07 02:29:18 +0530

    add create function to return image url

--- server/authentication/serializers.py
+++ server/authentication/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 from .models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(max_length = 100)
-    password = serializers.CharField(write_only=True) # exluded when returning the user
+    email = serializers.EmailField(max_length=100)
+    password = serializers.CharField(write_only=True)  # Excluded when returning the user
+    profile_image = serializers.ImageField(required=False)
+
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
+        fields = ["email", "username", "password", "profile_image"]
+
+    def create(self, validated_data):
+        profile_image = validated_data.pop('profile_image', None)
+        user = User.objects.create(**validated_data)
+        if profile_image:
+            user.profile_image = profile_image
+            user.save()
+        return user
