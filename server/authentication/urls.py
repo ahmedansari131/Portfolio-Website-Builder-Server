e018c0df2d14c734edff0cd75b353bfa8e33c4b7
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-06 23:38:54 +0530

    add url for email verification

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -3,4 +3,5 @@ from .views import AuthenticateUser
 
 urlpatterns = [
     path("register/", AuthenticateUser.as_view(), name="registeration"),
+    path("verify-email/", AuthenticateUser.as_view(), name="email_verification"),
 ]
