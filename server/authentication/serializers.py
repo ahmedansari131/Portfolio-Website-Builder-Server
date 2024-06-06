720717633b7003d5aa355f94d10a063eaf8892ee
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-07 01:24:10 +0530

    exclude password from user while returning

--- server/authentication/serializers.py
+++ server/authentication/serializers.py
@@ -4,6 +4,7 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length = 100)
+    password = serializers.CharField(write_only=True) # exluded when returning the user
     class Meta:
         model = User
-        fields = ['email', 'username']
+        fields = ['email', 'username', 'password']
