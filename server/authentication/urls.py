1cad48a6698cf3e294119c6819541719ab44b9de
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-07 22:11:03 +0530

    refactor: change view names

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AuthenticateUser
+from .views import UserRegistration, UserLogin, UserEmailVerification
 
 urlpatterns = [
-    path("register/", AuthenticateUser.as_view(), name="registeration"),
-    path("verify-email/", AuthenticateUser.as_view(), name="email_verification"),
+    path("register/", UserRegistration.as_view(), name="registeration"),
+    path("verify-email/", UserEmailVerification.as_view(), name="email_verification"),
+    path("login/", UserLogin.as_view(), name="login"),
 ]
