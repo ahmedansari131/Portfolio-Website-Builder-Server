f0e2e09a382c275d4537d9868da318411ade9069
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-09 02:17:25 +0530

    add url for reset password

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegistration, UserLogin, UserEmailVerification
+from .views import UserRegistration, UserLogin, UserEmailVerification, ResetPassword
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,4 +14,5 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('reset-password/', ResetPassword.as_view(), name='reset_password'),
 ]
