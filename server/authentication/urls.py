ecc7e171b94e3fee031e7ca945bcb68921c2ad48
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-10 21:44:12 +0530

    add change password url

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import UserRegistration, UserLogin, UserEmailVerification, ResetPassword, ForgotPassword
+from .views import (
+    UserRegistration,
+    UserLogin,
+    UserEmailVerification,
+    ResetPassword,
+    ForgotPassword,
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView
+    TokenVerifyView,
 )
 
 
@@ -13,8 +19,9 @@ urlpatterns = [
     path("login/", UserLogin.as_view(), name="login"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('reset-password/', ResetPassword.as_view(), name='reset_password'),
-    path('forgot-password/', ForgotPassword.as_view(), name='forgot_password'),
-    path('verify-user/', ForgotPassword.as_view(), name='verify_user'),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("reset-password/", ResetPassword.as_view(), name="reset_password"),
+    path("forgot-password/", ForgotPassword.as_view(), name="forgot_password"),
+    path("verify-user/", ForgotPassword.as_view(), name="verify_user"),
+    path("change-password/", ForgotPassword.as_view(), name="change_password"),
 ]
