7ebe0f8628667f8bdca044f75f845396a1244cf5
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-14 23:36:26 +0530

    remove print statement

--- server/server/utils/exception_handler.py
+++ server/server/utils/exception_handler.py
@@ -4,7 +4,6 @@ from server.response.api_response import ApiResponse
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    print("This is response -> ", response.data)
 
     if response is not None:
         return ApiResponse.response_failed(
