09bda1fd4422a41372361d2a002d22e6ccb156cb
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-14 21:05:17 +0530

    inherit permission mixin class

--- server/authentication/models.py
+++ server/authentication/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from cloudinary.models import CloudinaryField
 
 
@@ -30,7 +30,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         verbose_name="Email",
         max_length=100,
