69020b94ce23c7f4be23936cd323f9a0341bb980
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-22 16:34:25 +0530

    add more error handlers for not authenticated and not authorized

--- server/server/utils/exception_handler.py
+++ server/server/utils/exception_handler.py
@@ -1,13 +1,20 @@
 from rest_framework.views import exception_handler
 from server.response.api_response import ApiResponse
+from rest_framework import exceptions
 
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-
     if response is not None:
+        if isinstance(exc, exceptions.NotAuthenticated):
+            return ApiResponse.response_failed(
+                message="Authentication credentials were not provided.", status=401
+            )
+        elif isinstance(exc, exceptions.PermissionDenied):
+            return ApiResponse.response_failed(
+                message="You do not have permission to perform this action.", status=403
+            )
         return ApiResponse.response_failed(
             status=response.status_code, message=response.data
         )
-
     return response
