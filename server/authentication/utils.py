39a034e9c11d4c162196d343db5655f747ae506a
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-24 01:42:11 +0530

    add generate otp and set cookie helper

--- server/authentication/utils.py
+++ server/authentication/utils.py
@@ -1,8 +1,11 @@
+from django.http import JsonResponse
 from .models import User
 from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework_simplejwt.tokens import UntypedToken
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 import os
+import secrets
+
 
 def get_existing_user(user_id):
     try:
@@ -27,3 +30,23 @@ def verify_simple_jwt(token):
         return valid_data
     except TokenError as e:
         raise InvalidToken(e)
+
+
+def generate_otp(length=6):
+    otp = "".join([str(secrets.randbelow(10)) for _ in range(length)])
+    return otp
+
+
+def set_cookie_helper(
+    response, key, value, life, path="/", httponly=False, samesite="None", secure=True
+):
+    response.set_cookie(
+        key,
+        value,
+        max_age=life,
+        path=path,
+        httponly=httponly,
+        samesite=samesite,
+        secure=secure,
+    )
+    return response
