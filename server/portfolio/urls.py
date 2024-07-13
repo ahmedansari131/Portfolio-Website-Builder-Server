7777944b16d90ffee1b98225468482be68aeafe1
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-07-13 20:59:40 +0530

    add urls for uploading and listing all the templates

--- server/portfolio/urls.py
+++ server/portfolio/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import Project
+from .views import Project, UploadTemplate ,ListTemplates
 
 urlpatterns = [
-    path(
-        "create-project/", Project.as_view(), name="create_project"
-    ),
+    path("create-project/", Project.as_view(), name="create_project"),
+    path("upload-template/<template_name>/", UploadTemplate.as_view(), name="upload_template"),
+    path("list-templates/", ListTemplates.as_view(), name="list_templates"),
 ]
