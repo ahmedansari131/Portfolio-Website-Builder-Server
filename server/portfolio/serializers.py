510cf5c803c504cb955e14a0f262212aec36da1d
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-07-14 23:02:56 +0530

    add validation for project name uniqueness

--- server/portfolio/serializers.py
+++ server/portfolio/serializers.py
@@ -1,23 +1,26 @@
 from rest_framework import serializers
 from .models import PortfolioProject, Template
 from authentication.serializers import UserSerializer
+from server.utils.response import BaseResponse
 
 
 class CreateProjectSerializer(serializers.ModelSerializer):
     project_name = serializers.CharField(max_length=50)
-    template_id = serializers.CharField(max_length=100)
-    s3_file_name = serializers.CharField(max_length=50)
-    s3_folder_name = serializers.CharField(max_length=50)
-    bucket_name = serializers.CharField(max_length=50)
+    template_name = serializers.CharField(max_length=100)
 
     class Meta:
         model = PortfolioProject
-        fields = ["project_name", "template_id", "s3_file_name", "s3_folder_name", "bucket_name"]
+        fields = ["project_name", "template_name"]
+
+    def validate_project_name(self, value):
+        if PortfolioProject.objects.filter(project_name=value).exists():
+            return BaseResponse.error(message="Project with this name already exists")
+        return value
 
 
 class ListTemplatesSerializer(serializers.ModelSerializer):
     created_by = UserSerializer()
-    
+
     class Meta:
         model = Template
         fields = "__all__"
