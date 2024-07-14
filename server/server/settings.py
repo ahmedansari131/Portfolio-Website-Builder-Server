a83e881504bcdfbe384374de420a1323615206c5
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-07-14 23:04:27 +0530

    remove cookie middleware

--- server/server/settings.py
+++ server/server/settings.py
@@ -40,7 +40,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "authentication.middleware.CookieMiddleware",
     "authentication.middleware.LogIPAddressAndUserAgentMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
