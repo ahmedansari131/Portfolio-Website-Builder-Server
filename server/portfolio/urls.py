f20a56c637d43cab9354f22ae971bbb4a15fff54
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-07-20 01:58:31 +0530

    add url for listing project

--- server/portfolio/urls.py
+++ server/portfolio/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Project, UploadTemplate, ListTemplates
+from .views import Project, UploadTemplate, ListTemplates, ListPortfolioProject
 
 urlpatterns = [
     path("create-project/", Project.as_view(), name="create_project"),
@@ -14,4 +14,9 @@ urlpatterns = [
         name="upload_template",
     ),
     path("list-templates/", ListTemplates.as_view(), name="list_templates"),
+    path(
+        "list-portfolio-projects/",
+        ListPortfolioProject.as_view(),
+        name="list_portfolio_projects",
+    ),
 ]
