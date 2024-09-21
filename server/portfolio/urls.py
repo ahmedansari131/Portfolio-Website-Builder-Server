726c6e1fd34856f922a9e5b3215ab760b4f39308
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-09-21 23:26:51 +0530

    add url for updating site data and deploy site

--- server/portfolio/urls.py
+++ server/portfolio/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Project, UploadTemplate, ListTemplates, ListPortfolioProject
+from .views import Project, UploadTemplate, ListTemplates, ListPortfolioProject, UpdateCustomizeTemplate, Deployment
 
 urlpatterns = [
     path("create-project/", Project.as_view(), name="create_project"),
@@ -18,5 +18,15 @@ urlpatterns = [
         "list-portfolio-projects/",
         ListPortfolioProject.as_view(),
         name="list_portfolio_projects",
+    ),
+    path(
+        "update-dom-data/",
+        UpdateCustomizeTemplate.as_view(),
+        name="update_dom_data",
+    ),
+    path(
+        "deploy-portfolio/",
+        Deployment.as_view(),
+        name="deploy_portfolio",
     ),
 ]
