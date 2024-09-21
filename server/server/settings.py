f8c6f3a9fad6240ff6276679388f33289690d49c
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-09-21 23:12:33 +0530

    add variable for deployed bucket name

--- server/server/settings.py
+++ server/server/settings.py
@@ -156,6 +156,7 @@ cloudinary.config(
 AWS_ACCESS_KEY_ID = os.environ.get("S3_SECRET_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = os.environ.get("S3_KEY_ID")
 AWS_STORAGE_TEMPLATE_BUCKET_NAME = os.environ.get("S3_TEMPLATE_BUCKET_NAME")
+AWS_DEPLOYED_PORTFOLIO_BUCKET_NAME = os.environ.get("S3_DEPLOYED_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.environ.get("S3_REGION_NAME")
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
