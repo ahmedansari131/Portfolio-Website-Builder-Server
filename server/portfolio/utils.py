4559323e5a5eec99ff0f2b11ecc91343ffeca32c
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-10-28 19:55:01 +0530

    refactor: change variable name

--- server/portfolio/utils.py
+++ server/portfolio/utils.py
@@ -27,31 +27,29 @@ def generate_random_number(digits=6):
     return random_string
 
 
-def upload_image_on_s3_project(
-    image, project_folder_name, new_image_name, content_type
-):
+def upload_project_file_on_s3_project(file, project_folder_name, new_file_name):
     s3_client = s3_config()
     bucket_name = settings.AWS_DEPLOYED_PORTFOLIO_BUCKET_NAME
 
-    file_extension = os.path.splitext(image.name)[1]  # Extracts .png, .jpg, etc.
+    file_extension = os.path.splitext(file.name)[1]  # Extracts .png, .jpg, etc.
     if not file_extension:
-        file_extension = "." + image.content_type.split("/")[1]
-    image_s3_path = f"{project_folder_name.replace(' ', '-').lower()}/assests/{new_image_name}{file_extension}"
+        file_extension = "." + file.content_type.split("/")[1]
+    file_s3_path = f"{project_folder_name.replace(' ', '-').lower()}/assests/{new_file_name}{file_extension}"
 
     try:
         # Check if the object exists in S3
-        s3_client.head_object(Bucket=bucket_name, Key=image_s3_path)
+        s3_client.head_object(Bucket=bucket_name, Key=file_s3_path)
         # If it exists, delete it
-        print(f"{image_s3_path} already exists. Deleting it.")
-        s3_client.delete_object(Bucket=bucket_name, Key=image_s3_path)
+        print(f"{file_s3_path} already exists. Deleting it.")
+        s3_client.delete_object(Bucket=bucket_name, Key=file_s3_path)
     except ClientError as error:
         # Check if the error is because the object does not exist
         if error.response["Error"]["Code"] == "404":
-            print(f"{image_s3_path} does not exist in S3. No need to delete.")
+            print(f"{file_s3_path} does not exist in S3. No need to delete.")
         else:
-            print("Error occurred while checking if image already exists:", error)
+            print("Error occurred while checking if file already exists:", error)
             return BaseResponse.error(
-                message="Error occurred while checking if image already exists"
+                message="Error occurred while checking if file already exists"
             )
     except Exception as error:
         print("An unexpected error occurred:", error)
@@ -61,16 +59,18 @@ def upload_image_on_s3_project(
 
     try:
         s3_client.upload_fileobj(
-            image,
+            file,
             bucket_name,
-            image_s3_path,
-            ExtraArgs={"ContentType": image.content_type},
+            file_s3_path,
+            ExtraArgs={"ContentType": file.content_type},
         )
         invalidate_cloudfront_cache(
-            project_name=project_folder_name, file_name=new_image_name + file_extension
+            project_name=project_folder_name, file_name=new_file_name + file_extension
         )
-        distribution_id = os.environ.get("DEPLOYED_SITE_CLOUDFRONT_DISTRIBUION_ID")
-        url = f"https://{get_cloudfront_domain(distribution_id=distribution_id)}/{image_s3_path}"
+        # distribution_id = os.environ.get("DEPLOYED_SITE_CLOUDFRONT_DISTRIBUION_ID")
+
+        uploaded_file_sub_path = f"assests/{new_file_name}{file_extension}"
+        url = f'https://{project_folder_name}.{os.environ.get("DOMAIN_NAME")}/{uploaded_file_sub_path}'
         return {"image_s3_url": url}
     except Exception as error:
         print("Error occurred while uploading the project image on s3 -> ", error)
