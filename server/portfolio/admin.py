6b810cd2c1479a031ccace1af5d002bbf6c5b92e
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-10-28 19:53:11 +0530

    configure admin panel for portfolio project and deleted project

--- server/portfolio/admin.py
+++ server/portfolio/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
-from .models import Template, CustomizedTemplate, PortfolioProject
+from .models import (
+    Template,
+    CustomizedTemplate,
+    PortfolioProject,
+    DeletedPortfolioProject,
+)
 
 admin.site.register(Template)
 admin.site.register(CustomizedTemplate)
-admin.site.register(PortfolioProject)
+
+
+class PortfolioProjectAdmin(admin.ModelAdmin):
+    list_display = ["project_name", "is_deleted"]
+
+    def get_queryset(self, request):
+        # Only show items marked as deleted
+        return PortfolioProject.all_objects.filter(is_deleted=False)
+
+
+admin.site.register(PortfolioProject, PortfolioProjectAdmin)
+
+
+class DeletedPortfolioProjectAdmin(admin.ModelAdmin):
+    list_display = ["project_name", "is_deleted"]
+
+    def get_queryset(self, request):
+        # Only show items marked as deleted
+        return PortfolioProject.all_objects.filter(is_deleted=True)
+
+    # Optional: Action to recover deleted projects
+    actions = ["recover_projects"]
+
+    def recover_projects(self, request, queryset):
+        queryset.update(is_deleted=False)
+        self.message_user(request, "Selected projects have been recovered.")
+
+    recover_projects.short_description = "Recover selected deleted projects"
+
+
+admin.site.register(DeletedPortfolioProject, DeletedPortfolioProjectAdmin)
