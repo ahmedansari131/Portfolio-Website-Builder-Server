de7c418432a725841059aca6c910299505d91ee0
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-10-28 19:51:09 +0530

    refactor: add class for email verification

--- server/authentication/jwt_token.py
+++ server/authentication/jwt_token.py
@@ -6,15 +6,66 @@ from .models import User
 from .serializers import MyTokenObtainPairSerializer
 from django.http.response import JsonResponse
 from django.conf import settings
-from .constants import CHANGE_FORGOT_PASSWORD, DIRECT_LOGIN
+from .constants import (
+    CHANGE_FORGOT_PASSWORD,
+    DIRECT_LOGIN,
+    EMAIL_VERIFICATION_TOKEN_TYPE,
+)
 from rest_framework_simplejwt.tokens import RefreshToken
+from portfolio.models import PortfolioProject
+
+
+class EmailVerification:
+    @staticmethod
+    def portfolio_contact_email_verification(decoded_token):
+        try:
+            user_id = decoded_token.get("id")
+            project_id = decoded_token.get("project_id")
+            portfolio_project_instance = PortfolioProject.objects.get(
+                id=project_id, created_by=user_id
+            )
+            portfolio_project_instance.is_verified_portfolio_contact_email = True
+            portfolio_project_instance.save()
+
+            return "Portfolio contact email is now verified."
+        except jwt.ExpiredSignatureError:
+            return "Email verification link is expired."
+        except jwt.InvalidTokenError:
+            return "Email verification token is invalid"
+        except Exception as error:
+            return str(error)
+
+    @staticmethod
+    def auth_email_verification(decoded_token, token_type):
+        try:
+            user_instance = User.objects.filter(id=decoded_token.get("id")).first()
+            if user_instance:
+                if user_instance.is_active and (
+                    token_type == DIRECT_LOGIN or token_type == CHANGE_FORGOT_PASSWORD
+                ):
+                    return decoded_token
+                else:
+                    user_instance.is_active = True
+                    user_instance.save()
+                    return f"{user_instance.username} is verified and can login."
+
+        except jwt.ExpiredSignatureError:
+            if token_type == DIRECT_LOGIN or token_type == CHANGE_FORGOT_PASSWORD:
+                return (
+                    "Your request for forgot password is expired. Please request again!"
+                )
+            return ""
+        except jwt.InvalidTokenError:
+            return "Token is invalid."
+        except Exception as error:
+            return str(error)
 
 
 class Token:
-
-    def __init__(self, user_id=None, token_type=None):
+    def __init__(self, user_id=None, token_type=None, **kwargs):
         self.user_id = user_id
         self.token_type = token_type
+        self.additional_data = kwargs
 
     def generate_token(self):
         if not self.user_id:
@@ -29,6 +80,7 @@ class Token:
                 "id": self.user_id,
                 "exp": expiration_time,
                 "token_type": self.token_type,
+                **self.additional_data,
             }
             encoded_token = jwt.encode(
                 data,
@@ -44,18 +96,31 @@ class Token:
             decoded_token = jwt.decode(
                 token, os.environ.get("VERIFICATION_EMAIL_SECRET"), algorithms="HS256"
             )
-            user = User.objects.filter(id=decoded_token.get("id")).first()
-
-            if user and user.is_active:
-                if (
-                    self.token_type == DIRECT_LOGIN
-                    or self.token_type == CHANGE_FORGOT_PASSWORD
-                ):
-                    return decoded_token
-                return (
-                    f"{user.username}'s is already has an active account and can login."
+            token_type = decoded_token.get("token_type")
+            if token_type == EMAIL_VERIFICATION_TOKEN_TYPE:
+                verify_contact_email = (
+                    EmailVerification.portfolio_contact_email_verification(
+                        decoded_token
+                    )
                 )
-            return decoded_token
+                return verify_contact_email
+            else:
+                verification = EmailVerification.auth_email_verification(
+                    decoded_token=decoded_token, token_type=self.token_type
+                )
+                return verification
+
+            # user = User.objects.filter(id=decoded_token.get("id")).first()
+            # if user and user.is_active:
+            #     if (
+            #         self.token_type == DIRECT_LOGIN
+            #         or self.token_type == CHANGE_FORGOT_PASSWORD
+            #     ):
+            #         return decoded_token
+            #     return (
+            #         f"{user.username}'s is already has an active account and can login."
+            #     )
+            # return decoded_token
         except jwt.ExpiredSignatureError:
             if (
                 self.token_type == DIRECT_LOGIN
@@ -68,7 +133,7 @@ class Token:
         except jwt.InvalidTokenError:
             return "Invalid token"
         except Exception as error:
-            return "Error occurred on server while decoding verification token"
+            return str(error)
 
     def simple_jwt_response(self, tokens):
         response = JsonResponse({"status": 200})
