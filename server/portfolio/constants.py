d3ce1fcfaf14eb3078538a38fb939c07fff286ee
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-10-30 14:42:55 +0530

    add some constants

---
+++ server/portfolio/constants.py
@@ -0,0 +1,15 @@
+ELEMENT_DEFAULT_CLASS_NAME = 'brand-me'
+DATA_ASSET_ID_ATTR = 'data-assest-id'
+ELEMENT_IDENTIFIER = 'data-element-id'
+
+DOCUMENT_META_ELEMENTS = ['meta', 'link', 'script', 'title', 'style']
+
+S3_ASSETS_FOLDER_NAME = 'assets'
+S3_CSS_FOLDER_NAME = 'css'
+S3_JS_FOLDER_NAME = 'js'
+
+INDEX_FILE = 'index.html'
+ROOT_STYLE_FILE = 'style.css'
+UNIVERSAL_STYLE_FILE = 'body-style.css'
+ROOT_JS_FILE = 'script.js'
+TEMPLATE_PREVIEW_IMAGE = 'preview.png'
