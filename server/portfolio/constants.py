b8e637a09801edc5adcbcb33000682601ae48b6e
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-11-01 00:58:04 +0530

    refactor: add comments and improve constant name

--- server/portfolio/constants.py
+++ server/portfolio/constants.py
@@ -1,3 +1,5 @@
+
+# Element Custom Attributes and Values 
 ELEMENT_DEFAULT_CLASS_NAME = 'brand-me'
 ASSET_ID_PREFIX = 'data-assest-id'
 ASSET_ID_VALUE = 'asset'
@@ -7,12 +9,14 @@ ELEMENT_IDENTIFIER_VALUE = 'element'
 
 DOCUMENT_META_ELEMENTS = ['meta', 'link', 'script', 'title', 'style']
 
+# S3 Template Folders
 S3_ASSETS_FOLDER_NAME = 'assets'
 S3_CSS_FOLDER_NAME = 'css'
 S3_JS_FOLDER_NAME = 'js'
 
+# S3 Template Files
 INDEX_FILE = 'index.html'
 ROOT_STYLE_FILE = 'style.css'
-UNIVERSAL_STYLE_FILE = 'body-style.css'
+UNIVERSAL_STYLE_FILE = 'universal-style.css'
 ROOT_JS_FILE = 'script.js'
 TEMPLATE_PREVIEW_IMAGE = 'preview.png'
