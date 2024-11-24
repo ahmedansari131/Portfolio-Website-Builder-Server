84663dd513ffb8603422df6f710fc172908cdc8f
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-11-24 22:18:45 +0530

    remove unnecessary code

--- server/authentication/middleware.py
+++ server/authentication/middleware.py
@@ -1,34 +1,3 @@
-from django.utils.functional import SimpleLazyObject
-from django.contrib.auth.middleware import get_user
-from .utils import verify_simple_jwt
-from rest_framework_simplejwt.tokens import UntypedToken
-
-
-def get_user_jwt(request):
-    user = get_user(request)
-    return user
-
-
-class JWTAndCookieAuthenticationMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        access_token = request.COOKIES.get("access")
-        if access_token:
-            try:
-                UntypedToken(access_token)  # Verifies the token
-                request.user = SimpleLazyObject(lambda: get_user_jwt(request))
-            except Exception as error:
-                print("error ->", error)
-                request.user = SimpleLazyObject(lambda: None)
-        else:
-            request.user = SimpleLazyObject(lambda: get_user_jwt(request))
-        
-        response = self.get_response(request)
-        return response
-
-
 class LogIPAddressAndUserAgentMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
