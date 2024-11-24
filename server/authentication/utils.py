20c4878a30d20f57b4c797e9e9a3b183e56894f6
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-11-24 22:20:21 +0530

    add utility func for getting google access token

--- server/authentication/utils.py
+++ server/authentication/utils.py
@@ -1,4 +1,3 @@
-from django.http import JsonResponse
 from .models import User
 from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework_simplejwt.tokens import UntypedToken
@@ -6,7 +5,9 @@ from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 import os
 import secrets
 from .jwt_token import Token
-from authentication.constants import EMAIL_VERIFICATION_TOKEN_TYPE
+from django.conf import settings
+import requests
+from rest_framework.validators import ValidationError
 
 
 def get_existing_user(user_id):
@@ -40,7 +41,7 @@ def generate_otp(length=6):
 
 
 def set_cookie_helper(
-    response, key, value, life, path="/", httponly=False, samesite="None", secure=True
+    response, key, value, life, path="/", httponly=True, samesite="None", secure=True
 ):
     response.set_cookie(
         key,
@@ -62,3 +63,22 @@ def generate_email_verification_link(user_id, token_type, **kwargs):
             f'{os.environ.get("CLIENT_PATH_PREFIX")}/verify-email/?token={token}'
         )
         return verification_link
+
+
+def google_get_access_token(*, code: str, redirect_uri: str) -> str:
+    data = {
+        "code": code,
+        "client_id": settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY,
+        "client_secret": settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET,
+        "redirect_uri": redirect_uri,
+        "grant_type": "authorization_code",
+    }
+
+    response = requests.post("https://oauth2.googleapis.com/token", data=data)
+
+    if not response.ok:
+        raise ValidationError("Failed to obtain access token from Google.")
+
+    access_token = response.json()
+
+    return access_token
